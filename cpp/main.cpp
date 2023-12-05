958dd8b24ad613056e188e20d982712b567f8271
Shintaro Sakoda <sakoda@keio.jp>
2023-12-06 08:38:58 +0900

    Added get_active_window
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,6 +9,13 @@
 #include <X11/Xutil.h>
 // clang-format on
 
+struct Rect {
+  int x;
+  int y;
+  int width;
+  int height;
+};
+
 void move_cursor(Display *display, int x, int y) {
   std::cerr << "move_cursor (" << x << ", " << y << ")" << std::endl;
   XWarpPointer(display, None, DefaultRootWindow(display), 0, 0, 0, 0, x, y);
@@ -39,6 +46,23 @@ cv::Mat get_screenshot(Display *display, int x, int y, int width, int height) {
   return mat;
 }
 
+Window get_active_window(Display *display) {
+  Window focused;
+  int revert;
+  XGetInputFocus(display, &focused, &revert);
+
+  XWindowAttributes attrs;
+  XGetWindowAttributes(display, focused, &attrs);
+
+  return focused;
+}
+
+Rect get_window_rect(Display *display, Window window) {
+  XWindowAttributes attrs;
+  XGetWindowAttributes(display, window, &attrs);
+  return Rect{attrs.x, attrs.y, attrs.width, attrs.height};
+}
+
 int main() {
   Display *display = XOpenDisplay(nullptr);
 
@@ -46,6 +70,13 @@ int main() {
     std::cout << "Cannot open display" << std::endl;
     return 1;
   }
+
+  Window window = get_active_window(display);
+  Rect rect = get_window_rect(display, window);
+
+  std::cerr << "window: " << window << std::endl;
+  std::cerr << "rect: (" << rect.x << ", " << rect.y << ", " << rect.width
+            << ", " << rect.height << ")" << std::endl;
 
   for (int64_t i = 0; i < 5; i++) {
     move_cursor(display, 10, 10);
