0bb2f6a9f74d79e2581ce7115e1aeac9a0f8c239
Shintaro Sakoda <sakoda@keio.jp>
2023-12-06 13:50:21 +0900

    cv_mat_to_tensorのバグを修正
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- cpp/cv_mat_to_tensor.hpp
+++ cpp/cv_mat_to_tensor.hpp
@@ -9,16 +9,9 @@ torch::Tensor cv_mat_to_tensor(const cv::Mat & cv_img)
   const int height = cv_img.rows;
   const int width = cv_img.cols;
   const int channels = cv_img.channels();
-
-  std::vector<uint8_t> data(height * width * channels);
-
-  // cv::Matデータをvectorにコピー
-  memcpy(&data[0], cv_img.data, sizeof(uint8_t) * data.size());
-
-  // torch::Tensorを作成
-  torch::Tensor tensor_img = torch::from_blob(&data[0], {height, width, channels});
-  tensor_img = tensor_img.permute({2, 0, 1});   // channels firstに並び替え
-  tensor_img = tensor_img.to(torch::kFloat32);  // float32型に変換
-
-  return tensor_img.clone();
+  torch::Tensor tensor_img =
+    torch::from_blob(cv_img.data, {height, width, channels}, torch::kByte).clone();
+  tensor_img = tensor_img.to(torch::kFloat32) / 255.0;
+  tensor_img = tensor_img.permute({2, 0, 1});
+  return tensor_img;
 }
