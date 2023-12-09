f7cbba00b38f027b810a57adc5927626b40536c4
Shintaro Sakoda <sakoda@keio.jp>
2023-12-09 17:20:44 +0900

    Added parse_info_tsv.py
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ python/parse_info_tsv.py
@@ -0,0 +1,17 @@
+""" info.tsvから報酬を得た量を見る
+"""
+
+import argparse
+import pandas as pd
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('info_tsv', type=str)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    df = pd.read_csv(args.info_tsv, sep='\t')
+    print(f"合計 {df['reward'].sum()}")
