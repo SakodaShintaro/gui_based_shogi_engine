2602f8ca2eb6b8bc2dc0ee4baabd1dce2904bc10
Shintaro Sakoda <sakoda@keio.jp>
2023-12-17 17:02:17 +0900

    Fixed to actor-critic
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- cpp/main_functions/solve_grid_world_by_table.cpp
+++ cpp/main_functions/solve_grid_world_by_table.cpp
@@ -5,14 +5,13 @@ int main()
   const int64_t kGridSize = 3;
   GridWorld grid(kGridSize);
 
-  // Qテーブルの初期化
+  // テーブルの初期化
   const int64_t kTableSize = kGridSize * kGridSize * kGridSize * kGridSize;
-  torch::Tensor q_table = torch::zeros({kTableSize, kActionSize});
+  torch::Tensor policy_table = torch::zeros({kTableSize, kActionSize}).requires_grad_(true);
+  torch::Tensor value_table = torch::zeros({kTableSize});
 
-  std::mt19937_64 engine(std::random_device{}());
-  std::uniform_real_distribution<float> dist(0.0, 1.0);
-  std::uniform_int_distribution<int64_t> dist_action(0, kActionSize - 1);
-  constexpr float kEpsilon = 0.1;
+  torch::optim::SGD optimizer({policy_table}, 1.0);
+
   constexpr float kAlpha = 0.1;  // 学習率
   constexpr float kGamma = 0.9;  // 割引率
 
@@ -24,25 +23,35 @@ int main()
     // 現在の状態を取得
     const int64_t s = grid.state_as_int();
 
-    // ε-greedyに従って行動を選択
-    int64_t action;
-    if (dist(engine) < kEpsilon) {
-      action = dist_action(engine);
-    } else {
-      action = torch::argmax(q_table[s], 0).item<int64_t>();
-    }
+    // 行動選択
+    torch::Tensor policy = torch::softmax(policy_table[s], 0);
+    std::cout << policy << std::endl;
+    const int64_t action = torch::multinomial(policy, 1).item<int64_t>();
 
-    // 行動aを実行し、r, s'を観測
+    // 行動aを実行し、r, nsを観測
     const bool success = grid.step(static_cast<Action>(action));
     const float reward = (success ? 1.0 : -0.1);
-    const int64_t ns = grid.state_as_int();
     success_num += success;
 
     std::cout << "i = " << i << ", action = " << action << ", reward = " << reward
               << ", success_num = " << success_num << std::endl;
 
-    // Q関数を更新
-    const float max_future_q = torch::max(q_table[ns]).item<float>();
-    q_table[s][action] += kAlpha * (reward + kGamma * max_future_q - q_table[s][action]);
+    // 関数を更新
+    // 状態価値を更新
+    const int64_t ns = grid.state_as_int();
+    const float next_value = torch::max(value_table[ns]).item<float>();
+    torch::Tensor td = reward + kGamma * next_value - value_table[s];
+    std::cout << value_table[s].item<float>() << " ";
+    value_table[s] += kAlpha * td;
+    std::cout << value_table[s].item<float>() << std::endl;
+
+    // 損失を計算
+    torch::Tensor log_prob = torch::log_softmax(policy_table[s], 0)[action];
+    torch::Tensor actor_loss = -log_prob * td;
+
+    // 勾配降下で方策を更新
+    optimizer.zero_grad();
+    actor_loss.backward();
+    optimizer.step();
   }
 }
