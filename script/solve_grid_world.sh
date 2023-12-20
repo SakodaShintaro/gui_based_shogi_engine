7d34516b44edbcee4e83de0def85cac6613f5ad5
Shintaro Sakoda <sakoda@keio.jp>
2023-12-20 14:26:16 +0900

    Fixed name
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- script/solve_grid_world.sh
+++ script/solve_grid_world.sh
@@ -4,5 +4,5 @@ set -eux
 cd $(dirname $0)/../build-Release
 cmake ../ -DCMAKE_BUILD_TYPE=Release
 cmake --build ./ --config Release --target all --
-./solve_grid_world_by_table
+./solve_grid_world
 python3 ../python/plot_grid_world_log.py ./grid_world_log.tsv
