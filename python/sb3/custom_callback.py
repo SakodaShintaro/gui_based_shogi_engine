d15a5504ff609b015e6d3a5174a3ead471c5a9fc
Shintaro Sakoda <sakoda@keio.jp>
2023-12-23 17:44:25 +0900

    Fixed to used callback
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ python/sb3/custom_callback.py
@@ -0,0 +1,35 @@
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.env_util import make_vec_env
+from env_grid_world import CustomEnv
+import time
+
+
+class CustomCallback(BaseCallback):
+    def __init__(self):
+        super().__init__()
+        self.start_time = time.time()
+        self.vec_env = make_vec_env(
+            CustomEnv, n_envs=1, env_kwargs=dict(grid_size=4))
+
+    def _on_step(self) -> bool:
+        # Test
+        obs = self.vec_env.reset()
+        n_steps = 1000
+        ideal_num = 0
+        for step in range(n_steps):
+            action, _ = self.model.predict(obs, deterministic=False)
+            obs, reward, done, info = self.vec_env.step(action)
+            is_ideal_action = info[0].get('is_ideal_action')
+            # print(
+            #     f"step={step}, action={action}, reward={reward}, done={done}, is_ideal_action={is_ideal_action}")
+            # vec_env.render()
+            ideal_num += is_ideal_action
+
+        ideal_rate = 100 * ideal_num / n_steps
+        elapsed = time.time() - self.start_time
+        elapsed_str = time.strftime("%H:%M:%S", time.gmtime(elapsed))
+        total = self.locals["total_timesteps"]
+        progress = 100 * self.num_timesteps / total
+        print(
+            f"{elapsed_str}  num_timesteps={self.num_timesteps:7d}({progress:5.1f}%), ideal_rate={ideal_rate:5.1f}")
+        return True
--- python/sb3/main.py
+++ python/sb3/main.py
@@ -1,27 +1,8 @@
 from stable_baselines3 import PPO, A2C, DQN
-from stable_baselines3.common.env_util import make_vec_env
 from env_grid_world import CustomEnv
+from custom_callback import CustomCallback
+from stable_baselines3.common.callbacks import EveryNTimesteps
 
 env = CustomEnv(4)
-vec_env = make_vec_env(CustomEnv, n_envs=1, env_kwargs=dict(grid_size=4))
-model = A2C("CnnPolicy", env, verbose=0)
-
-for epoch in range(100):
-    # Train
-    model = model.learn(1000)
-
-    # Test
-    obs = vec_env.reset()
-    n_steps = 1000
-    ideal_num = 0
-    for step in range(n_steps):
-        action, _ = model.predict(obs, deterministic=False)
-        obs, reward, done, info = vec_env.step(action)
-        is_ideal_action = info[0].get('is_ideal_action')
-        # print(
-        #     f"step={step}, action={action}, reward={reward}, done={done}, is_ideal_action={is_ideal_action}")
-        # vec_env.render()
-        ideal_num += is_ideal_action
-
-    ideal_rate = 100 * ideal_num / n_steps
-    print(f"epoch={epoch:3d}, ideal_rate={ideal_rate:.1f}")
+event_callback = EveryNTimesteps(n_steps=1000, callback=CustomCallback())
+model = A2C("CnnPolicy", env, verbose=0).learn(100000, callback=event_callback)
