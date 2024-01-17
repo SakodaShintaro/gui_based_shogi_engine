9092874ed80c6db212920c7146cc61fc19e9f9fd
Shintaro Sakoda <sakoda@keio.jp>
2024-01-17 21:30:29 +0900

    コマンドを修正
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- python/bbf/install.sh
+++ python/bbf/install.sh
@@ -6,10 +6,8 @@ source .env/bin/activate
 
 pip3 install -r requirements.txt
 
-export PYTHONPATH="$(readlink -f ../):$PYTHONPATH"
-
 python3 -m bbf.train \
     --agent=BBF \
     --gin_files=bbf/configs/BBF.gin \
-    --base_dir=/tmp/online_rl/bbf \
+    --base_dir=./bbf_result \
     --run_number=1
